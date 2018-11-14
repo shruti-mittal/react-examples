c480a577e212a47fd70810bbac617c56c3abe88d
shruti-mittal <shrutimittal0404@gmail.com>
2018-11-14 17:15:52 +0530

    Access input data passed to the react component

--- hello-react.js
+++ hello-react.js
@@ -6,9 +6,9 @@ class HelloWorld extends React.Component
 {
     render()
     {
-        return el( 'h1', {style: {color:'gray', textAlign: 'center'}}, 'Hey!' );
+        return el( 'h1', {style: {color:'gray', textAlign: 'center'}}, 'Hi '+this.props.inputData+'!' );
     }
 }
 
 const domContainer1 = document.getElementById('react-example-one');
-ReactDOM.render( el( HelloWorld ),  domContainer1);
+ReactDOM.render( el( HelloWorld, {inputData: 'Learner'} ), domContainer1);
